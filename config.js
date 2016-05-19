224dcb470cc1ad72fda32077cf3995862058e118
John Zaccone <john.zaccone@gmail.com>
2016-05-19 12:59:39 -0400

    add johnzaccone.io to url for server config

--- config.js
+++ config.js
@@ -109,7 +109,7 @@ if (process.env.OPENSHIFT_MYSQL_DB_HOST != undefined) {
         // When running Ghost in the wild, use the production environment
         // Configure your URL and mail settings here
         production: {
-            url: 'http://'+process.env.OPENSHIFT_APP_DNS,
+            url: 'http://www.johnzaccone.io',
             mail: {},
             database: {
                 client: 'sqlite3',
